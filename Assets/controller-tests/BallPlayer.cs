c078708d0a333ce8c6c775cd90a137f0affd7303
Jake <jake.trefethen@gmail.com>
2018-03-17 13:02:27 -0700

    controller

--- Assets/controller-tests/BallPlayer.cs
+++ Assets/controller-tests/BallPlayer.cs
@@ -5,11 +5,15 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class BallPlayer : MonoBehaviour {
 
-    public float TurnDrag;
     public float Force;
     public JoystickCamera _cam;
+    public Transform Character;
+    [Range(10, 40)]
+    public float TurnDamping;
+    [Range(10, 40)]
+    public float SkidDamping;
     private Rigidbody _rigidbody;
-    private Vector3 _prevForce;
+    private Vector3 _force;
 
 
 	// Use this for initialization
@@ -18,17 +22,24 @@ public class BallPlayer : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
         transform.forward = _rigidbody.velocity;
         float v = CrossPlatformInputManager.GetAxis("Vertical"), h = CrossPlatformInputManager.GetAxis("Horizontal");
-        Vector3 force = (_cam.transform.forward * v + _cam.transform.right * h).normalized;
+        Vector3 targetForce = (_cam.transform.forward * v + _cam.transform.right * h).normalized;
         RaycastHit hit;
         if (Physics.Raycast(transform.position, Vector3.down, out hit, 5f)) {
-            force = Vector3.ProjectOnPlane(force, hit.normal);
+            targetForce = Vector3.ProjectOnPlane(targetForce, hit.normal);
         }
         float mag = _rigidbody.velocity.magnitude;
-        force *= Force * Time.deltaTime * mag;
-        _rigidbody.velocity += force;
+
+        targetForce *= Force * Time.deltaTime * mag;
+        _force = Vector3.Lerp(_force, targetForce, 1/TurnDamping);
+
+        _rigidbody.velocity += _force;
         _rigidbody.velocity = _rigidbody.velocity.normalized * mag;
+
+        Character.transform.forward = Vector3.Lerp(Character.transform.forward, targetForce.normalized, 1/SkidDamping);
+        Character.transform.position = transform.position;
+
 	}
 }
